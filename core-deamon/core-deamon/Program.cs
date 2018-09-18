bb4c264b7506926fa9cf12665724388bca1cd483
Steven DiBello <steve.dibello@Rovicorp.com>
2018-09-18 15:47:19 -0400

    serilog integraion

---
+++ core-deamon/core-deamon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.File;
+using Serilog.Sinks.SystemConsole;
+using Serilog.Configuration;
+using core_deamon.enricher;
+
+namespace core_deamon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.With(new threadId_logEventEnricher())
+                .Enrich.WithProperty("Version", "1.0.0")
+                .WriteTo.File("/var/log/core-deamon-.txt",
+                    rollingInterval: RollingInterval.Day,
+                    rollOnFileSizeLimit: true,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}"                
+                    )
+                .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Warning)
+                .CreateLogger();
+
+
+            Log.Information("Starting Service");
+            Log.Information("Hello Information");
+            Log.Debug("Hello Debug");
+
+            Log.CloseAndFlush();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ core-deamon/core-deamon/enricher/threadId_logEventEnricher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Serilog.Core;
+using Serilog.Events;
+using System.Threading;
+
+namespace core_deamon.enricher
+{
+    class threadId_logEventEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+                    "ThreadId", Thread.CurrentThread.ManagedThreadId));
+        }
+    }
+}
