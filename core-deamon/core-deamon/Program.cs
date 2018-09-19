a003d6e95726f1ef293e33ee8f13f14b2b5c9a16
steve dibello <steve.dibello@tivo.com>
2018-09-19 16:24:14 -0400

    changed the output Template for add version, and guid ( for tracing )

--- core-deamon/core-deamon/Program.cs
+++ core-deamon/core-deamon/Program.cs
@@ -4,25 +4,34 @@ using Serilog.Events;
 using Serilog.Sinks.File;
 using Serilog.Sinks.SystemConsole;
 using Serilog.Configuration;
+using Serilog.Context;
 using core_deamon.enricher;
 
 namespace core_deamon
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
                 .Enrich.With(new threadId_logEventEnricher())
+                .Enrich.FromLogContext()
                 .Enrich.WithProperty("Version", "1.0.0")
                 .WriteTo.File("/var/log/core-deamon-.txt",
                     rollingInterval: RollingInterval.Day,
                     rollOnFileSizeLimit: true,
-                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}"                
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} v{Version:lj} guid{GUID} [{Level:u3}] {Message:lj}{NewLine}{Exception}"                
                     )
                 .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
                 .CreateLogger();
+
+
+            using (LogContext.PushProperty("A", 1))
+            {
+                LogContext.PushProperty("GUID", Guid.NewGuid());
+                Log.Debug("Logging from a property context");
+            }
 
 
             Log.Information("Starting Service");
