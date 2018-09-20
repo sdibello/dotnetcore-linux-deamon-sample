5f90118384068e4d305d60aa69b9256114f400df
Steven DiBello <steve.dibello@Rovicorp.com>
2018-09-20 14:39:20 -0400

    fixing thread id enhancer

--- core-deamon/core-deamon/Program.cs
+++ core-deamon/core-deamon/Program.cs
@@ -4,6 +4,7 @@ using Serilog.Events;
 using Serilog.Sinks.File;
 using Serilog.Sinks.SystemConsole;
 using Serilog.Configuration;
+using Serilog.Enrichers;
 using Serilog.Context;
 using core_deamon.enricher;
 
@@ -17,28 +18,28 @@ namespace core_deamon
                 .MinimumLevel.Debug()
                 .Enrich.With(new threadId_logEventEnricher())
                 .Enrich.FromLogContext()
+                .Enrich.WithThreadId()
                 .Enrich.WithProperty("Version", "1.0.0")
                 .WriteTo.File("/var/log/core-deamon-.txt",
                     rollingInterval: RollingInterval.Day,
                     rollOnFileSizeLimit: true,
-                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} v{Version:lj} guid{GUID} [{Level:u3}] {Message:lj}{NewLine}{Exception}"                
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} v{Version:lj} [{Level:u3}] [{ThreadId}]({GUID})-{Message:lj}{NewLine}{Exception}"
                     )
                 .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
                 .CreateLogger();
 
 
-            using (LogContext.PushProperty("A", 1))
+            Log.Information("Starting Service");
+            using (LogContext.PushProperty("GUID", Guid.NewGuid()))
             {
-                LogContext.PushProperty("GUID", Guid.NewGuid());
                 Log.Debug("Logging from a property context");
             }
 
-
-            Log.Information("Starting Service");
             Log.Debug("Hello Debug");
 
-            Log.CloseAndFlush();
             Console.ReadLine();
+            Log.Information("Stopping Service");
+            Log.CloseAndFlush();
         }
     }
 }
